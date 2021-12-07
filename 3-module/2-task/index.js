78126a5a32087d977c29b02562647b23911744fc
Vladimir Kharin <goanaft@gmail.com>
2021-12-07 21:27:42 +0300

    3 module 1,2,3,4,5 tasks

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
   // ваш код...
+  if (a > b) [a, b] = [b, a];
+  let filtered = [];
+  
+  arr.forEach(item => {
+    if (item >=a && item <=b) filtered.push(item)
+  })
+  
+  return filtered
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
   // ваш код...
+  let min = 0;
+  let max = 0;
+  const arr = str.split(' ')
+    
+  arr.forEach(item => {
+
+      item = +item;
+      
+      if (item < min) min = item;
+      if (item > max) max = item
+  });
+
+  return { min , max}
 }
