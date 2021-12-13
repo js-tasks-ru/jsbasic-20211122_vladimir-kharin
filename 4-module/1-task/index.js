bc0d40c271362abeccae02d6ca96c503e2163d2f
Vladimir Kharin <goanaft@gmail.com>
2021-12-13 22:03:45 +0300

    4 module 123

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let myPart =  document.createElement("ul");
+     
+  for (let i=0; i < friends.length; i++) {
+
+    const friend = document.createElement("li");
+    friend.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    myPart.append(friend);
+
+  }
+
+  return myPart
+ 
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
   // ваш код...
+
+  for (let i = 0; i < table.tBodies[0].rows.length ; i++) {
+
+    const Row = table.tBodies[0].rows[i];
+    const Age = Row.cells[1];
+    const Gender = Row.cells[2];
+    const Status = Row.cells[3];
+
+    if (Age.innerHTML < 18) {
+      Row.style = 'text-decoration: line-through'
+    };
+
+    if (Gender.innerHTML == 'm') { 
+      Row.classList.add('male') 
+    } else {
+      Row.classList.add('female')
+    };
+
+    switch (Status.dataset.available) {
+      case "true":
+        Row.classList.add('available');
+        break;
+      case "false":
+        Row.classList.add('unavailable');
+        break;
+      case undefined:
+        Row.hidden = true;
+    };
+  }
+
 }
