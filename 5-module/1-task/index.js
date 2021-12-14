621264d26f8df20c119f2b473a1c8114b9bcbafa
Vladimir Kharin <goanaft@gmail.com>
2021-12-14 19:47:00 +0300

    5 module 123

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
 function hideSelf() {
   // ваш код...
+  const Btn = document.getElementsByClassName( 'hide-self-button' )[0].addEventListener( "click" , (e) => e.currentTarget.hidden = true);
+
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
   // ваш код...
+  document.getElementsByClassName('toggle-text-button')[0].addEventListener('click', ()=> {
+    const Text = document.getElementById('text');
+    Text.hidden = !Text.hidden;
+  })
 }
