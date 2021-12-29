b465a98346858715b2e13e16b9512fed3095762a
Vladimir Kharin <goanaft@gmail.com>
2021-12-29 11:14:15 +0300

    8 module 2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -4,6 +4,47 @@ import ProductCard from '../../6-module/2-task/index.js';
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
-    this.filters = {};
+    this.filters = {
+      noNuts: false, // true/false
+      vegeterianOnly: false, // true/false
+      maxSpiciness: 4, // числа от 0 до 4
+      category: '' // уникальный идентификатор категории товара
+    };
+
+    this.render();
+    this.updateFilter(this.filters);
+
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+          <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+        </div>
+      </div>
+    `)
+  }
+
+  updateFilter(filters) {
+    console.log('FiltersUpdate');
+
+    Object.assign(this.filters, filters);
+
+    let productsGridInner = this.elem.querySelector('.products-grid__inner');
+    productsGridInner.innerHTML = '';
+
+    for (const item of this.products) {
+
+      if (this.filters.noNuts) {if (item.nuts) continue};
+      if (this.filters.vegeterianOnly) {if (!item.vegeterian) continue};
+      if (item.spiciness > this.filters.maxSpiciness) continue;
+      if (this.filters.category !== '') {if (this.filters.category !== item.category) continue}
+      
+      let productCard = new ProductCard (item);
+      productsGridInner.append(productCard.elem)
+      
+    }
+    
   }
 }
